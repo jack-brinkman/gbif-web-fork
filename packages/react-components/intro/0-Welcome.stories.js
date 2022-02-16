1f1a7a440c0fdd4b52df1490772950b00cf8e4dd
Morten Høfft <mhoefft@gbif.org>
2022-02-16 11:52:13 +0100

    styling

--- packages/react-components/intro/0-Welcome.stories.js
+++ packages/react-components/intro/0-Welcome.stories.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { linkTo } from '@storybook/addon-links';
 import { StyledProse } from '../src/components/typography/StyledProse';
+import DocsWrapper from '../src/components/DocsWrapper';
 import { Button } from '../src/components';
 import readme from './README.md';
 
-export const Welcome = () => <div style={{padding: '20px 50px'}}>
-  <StyledProse source={readme}></StyledProse>
-  <Button onClick={linkTo('Search/OccurrenceSearch')}>Explore &apos;Occurrence search&apos;</Button>
-</div>
-{/* <Welcome showApp={linkTo('Button')} />; */}
+export const Welcome = () => <DocsWrapper style={{padding: '20px 50px'}}>
+  <StyledProse source={readme}>
+    <Button onClick={linkTo('Search/OccurrenceSearch')}>Explore &apos;Occurrence search&apos;</Button>
+  </StyledProse>
+</DocsWrapper>
 
 export default {
   title: 'Welcome',
--- packages/react-components/intro/README.md
+++ packages/react-components/intro/README.md
@@ -2,4 +2,6 @@
 
 A collection of components, widgets and larger search modules and full pages. The focus is currently to create a reusable occurrence search for GBIF.org and other sites.
 
-It is in a state of initial development.
+It is under development, but the occurrence search is already now being used in production.
+
+Breaking changes can still happen.
