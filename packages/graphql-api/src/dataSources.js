3b04b781896d8d5b471fd72dd378ad251bc79ee0
Jack Brinkman <jack.brinkman@csiro.au>
2022-09-05 12:07:23 +1000

    Modular test

--- packages/graphql-api/src/dataSources.js
+++ packages/graphql-api/src/dataSources.js
@@ -1,29 +1,33 @@
-const _ = require('lodash');
+const _ = require("lodash");
+const { graphql } = require("../../../../gql-resolvers");
 
 const api = _.merge(
-  require('./resources/dataset').dataSource,
-  require('./resources/organization').dataSource,
-  require('./resources/taxon').dataSource,
-  require('./resources/network').dataSource,
-  require('./resources/installation').dataSource,
-  require('./resources/node').dataSource,
-  require('./resources/participant').dataSource,
-  require('./resources/occurrence').dataSource,
-  require('./util/wikidata/').dataSource,
-  require('./resources/collection').dataSource,
-  require('./resources/institution').dataSource,
-  require('./resources/staffMember').dataSource,
-  require('./resources/external/orcid').dataSource,
-  require('./resources/external/viaf').dataSource,
-  require('./resources/external/person').dataSource,
-  
-  require('./resources/literature').dataSource,
-  require('./resources/download').dataSource,
-// -- Add imports above this line (required by plopfile.js) --
+  // require("./resources/dataset").dataSource,
+  graphql.dataset.dataSource,
+  require("./resources/organization").dataSource,
+  require("./resources/taxon").dataSource,
+  require("./resources/network").dataSource,
+  require("./resources/installation").dataSource,
+  require("./resources/node").dataSource,
+  require("./resources/participant").dataSource,
+  // require("./resources/occurrence").dataSource,
+  graphql.occurrence.dataSource,
+  require("./util/wikidata/").dataSource,
+  //require("./resources/collection").dataSource,
+  graphql.collection.dataSource,
+  require("./resources/institution").dataSource,
+  require("./resources/staffMember").dataSource,
+  require("./resources/external/orcid").dataSource,
+  require("./resources/external/viaf").dataSource,
+  require("./resources/external/person").dataSource,
+
+  require("./resources/literature").dataSource,
+  require("./resources/download").dataSource
+  // -- Add imports above this line (required by plopfile.js) --
 );
 
 // merge resolvers as suggeted in https://blog.apollographql.com/modularizing-your-graphql-schema-code-d7f71d5ed5f2
 // TODO perhaps we should add an alert of keys are used twice
 module.exports = {
-  api
+  api,
 };
--- packages/graphql-api/src/typeDefs.js
+++ packages/graphql-api/src/typeDefs.js
@@ -1,6 +1,7 @@
-const { gql } = require('apollo-server');
-const _ = require('lodash');
-const { getEnumTypeDefs } = require('./enums');
+const { gql } = require("apollo-server");
+const _ = require("lodash");
+const { getEnumTypeDefs } = require("./enums");
+const { graphql } = require("../../../../gql-resolvers/lib");
 
 async function getSchema() {
   // create a string with all the enum options (loaded from the API)
@@ -19,38 +20,41 @@ async function getSchema() {
 
   const typeDefs = _.flatten([
     rootQuery,
-    require('./input'),
-    require('./resources/misc/comment'),
-    require('./resources/misc/contact'),
-    require('./resources/misc/endpoint'),
-    require('./resources/misc/identifier'),
-    require('./resources/misc/machineTag'),
-    require('./resources/misc/tag'),
-    require('./resources/misc/address'),
-    require('./resources/dataset').typeDef,
-    require('./resources/organization').typeDef,
-    require('./resources/scalars').typeDef,
-    require('./resources/taxon').typeDef,
-    require('./resources/network').typeDef,
-    require('./resources/installation').typeDef,
-    require('./resources/node').typeDef,
-    require('./resources/participant').typeDef,
-    require('./resources/occurrence').typeDef,
-    require('./util/wikidata').typeDef,
-    require('./resources/collection').typeDef,
-    require('./resources/institution').typeDef,
-    require('./resources/staffMember').typeDef,
-    require('./resources/external/orcid').typeDef,
-    require('./resources/external/viaf').typeDef,
-    require('./resources/external/person').typeDef,
-    require('./resources/literature').typeDef,
-    require('./resources/download').typeDef,
-// -- Add imports above this line (required by plopfile.js) --
+    require("./input"),
+    require("./resources/misc/comment"),
+    require("./resources/misc/contact"),
+    require("./resources/misc/endpoint"),
+    require("./resources/misc/identifier"),
+    require("./resources/misc/machineTag"),
+    require("./resources/misc/tag"),
+    require("./resources/misc/address"),
+    // require("./resources/dataset").typeDef,
+    graphql.dataset.typeDef,
+    require("./resources/organization").typeDef,
+    require("./resources/scalars").typeDef,
+    require("./resources/taxon").typeDef,
+    require("./resources/network").typeDef,
+    require("./resources/installation").typeDef,
+    require("./resources/node").typeDef,
+    require("./resources/participant").typeDef,
+    // require("./resources/occurrence").typeDef,
+    graphql.occurrence.typeDef,
+    require("./util/wikidata").typeDef,
+    // require('./resources/collection').typeDef,
+    graphql.collection.typeDef,
+    require("./resources/institution").typeDef,
+    require("./resources/staffMember").typeDef,
+    require("./resources/external/orcid").typeDef,
+    require("./resources/external/viaf").typeDef,
+    require("./resources/external/person").typeDef,
+    require("./resources/literature").typeDef,
+    require("./resources/download").typeDef,
+    // -- Add imports above this line (required by plopfile.js) --
   ]);
 
   return typeDefs;
 }
 
 module.exports = {
-  getSchema
+  getSchema,
 };
